72a7896407b84a0802cd9106ef00d3d49cdaf9f5
vux1512 <83004198+vux1512@users.noreply.github.com>
2023-01-10 20:08:37 +0700

    Update ListCity.java

--- src/ListCity.java
+++ src/ListCity.java
@@ -1,25 +1,62 @@
-import java.io.Serializable;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
-public class ListCity implements Serializable{
-	private City[] cityArray = new City[8];
-	
-	public ListCity() {	
-	}
-	
-	public ListCity(City[] cityArray) {
-		this.cityArray = cityArray;
-	}
 
-	public City[] getCityArray() {
-		return cityArray;
-	}
+public class Server {
+	
+	public static void main(String[] args) {
+			Handle hd = new Handle();
+			byte[] byte_read = new byte[9999];
+			
+			try {	
+				ServerSocket serverSocket = new ServerSocket(6868);
+				Socket socket = serverSocket.accept();
+				
+				DataInputStream input = new DataInputStream(socket.getInputStream());
+				ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+				
+				Handle setDaNang = new Handle(); 
+				
+				do {
+					
+					String inputThoiTietView = input.readUTF();
+					
+					//Set Main
+					if (inputThoiTietView.equalsIgnoreCase("setMain") ) {
+						System.out.println("setMain");
+						ListCity cityArray = new ListCity();
+						for (int i=0; i<8; i++) {
+							City citySetMain = hd.getCityWeather(i);
+							cityArray.addCityArray(citySetMain, i);
+						}
+						output.writeObject((Object)cityArray);
+						output.flush();
+					
+					//Set MainRight
+					} else if (inputThoiTietView.equalsIgnoreCase("setMainRight") ) {
+						System.out.println(inputThoiTietView);
+						City citySetMainRight = hd.getDaNang();
+						output.writeObject((Object)citySetMainRight);
+						output.flush();
+						
+					//Set Search
+					} else {
+						City citySearch = hd.Search(inputThoiTietView);
+						output.writeObject((Object)citySearch);
+						output.flush();
+					}
 
-	public void setCityArray(City[] cityArray) {
-		this.cityArray = cityArray;
-	}
-	
-	public void addCityArray(City city, int index) {
-		this.cityArray[index] = city;
-	}
-	
+				} while (true);
+				
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}		
 }
