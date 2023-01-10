c8eda2c098323ed9527b64fb90b2a1be2794e3fe
vux1512 <83004198+vux1512@users.noreply.github.com>
2023-01-10 20:08:53 +0700

    Update ListCity.java

--- src/ListCity.java
+++ src/ListCity.java
@@ -1,62 +1,54 @@
-import java.io.DataInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Scanner;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.Normalizer;
+import java.util.regex.Pattern;
+import java.awt.Font;
+import javax.swing.Icon;
+import javax.swing.SwingConstants;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import javax.swing.JSeparator;
 
 
-public class Server {
-	
-	public static void main(String[] args) {
-			Handle hd = new Handle();
-			byte[] byte_read = new byte[9999];
-			
-			try {	
-				ServerSocket serverSocket = new ServerSocket(6868);
-				Socket socket = serverSocket.accept();
-				
-				DataInputStream input = new DataInputStream(socket.getInputStream());
-				ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
-				
-				Handle setDaNang = new Handle(); 
-				
-				do {
-					
-					String inputThoiTietView = input.readUTF();
-					
-					//Set Main
-					if (inputThoiTietView.equalsIgnoreCase("setMain") ) {
-						System.out.println("setMain");
-						ListCity cityArray = new ListCity();
-						for (int i=0; i<8; i++) {
-							City citySetMain = hd.getCityWeather(i);
-							cityArray.addCityArray(citySetMain, i);
-						}
-						output.writeObject((Object)cityArray);
-						output.flush();
-					
-					//Set MainRight
-					} else if (inputThoiTietView.equalsIgnoreCase("setMainRight") ) {
-						System.out.println(inputThoiTietView);
-						City citySetMainRight = hd.getDaNang();
-						output.writeObject((Object)citySetMainRight);
-						output.flush();
+public class ThoiTietMain extends JFrame implements ActionListener {
+	public ThoiTietMain() {
+	}
+
+	public void init() {		
+		try {	
+			ThoiTietView ttv = new ThoiTietView();
+			ttv.setSize(775,520);
+			ttv.setVisible(true);
+			ttv.setLocationRelativeTo(null);
 						
-					//Set Search
-					} else {
-						City citySearch = hd.Search(inputThoiTietView);
-						output.writeObject((Object)citySearch);
-						output.flush();
-					}
+			}catch(Exception e1) {
+			System.out.println("Không tìm thấy thành phố này");
+		}
+	}
 
-				} while (true);
-				
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
-			}
-		}		
+
+	public static void main(String[] args) {
+		
+		ThoiTietMain ttm = new ThoiTietMain();
+		ttm.init();
+		ttm.setTitle("Dự Báo Thời Tiết");
+		ttm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+
 }
